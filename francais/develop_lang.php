b466e5b753fd04578bbb36363e1c45b39064c79c
KeitIG <pierre.de.la.martiniere@gmail.com>
2014-05-02 12:45:37 +0200

    Update french translation

--- francais/develop_lang.php
+++ francais/develop_lang.php
@@ -2,8 +2,8 @@
 $lang['develop_title']                  = "Développez";
 $lang['develop_subtitle']               = "Vos idées en code";
 $lang['develop_parahead']               = "Ressources, Informations & Plus";
-$lang['develop_paragraph']              = "Basée sur les meilleures technologies libres, elementary est une plateforme spécialement adaptée pour les développeurs. Les meilleures ressources et documentations sont mises à disposition à tout développeur pour que les applications se démarquent réellement.";
-$lang['develop_link1']                  = "Consultez le guide de l'interface utilisateur (HIG) et apprenez comment votre application peut offrir une expérience inédite.";
+$lang['develop_paragraph']              = "Basée sur les meilleures technologies du libre, elementary est une plateforme spécialement adaptée pour les développeurs. Les meilleures ressources et documentations sont mises à disposition de tout développeur pour que vos applications se démarquent réellement.";
+$lang['develop_link1']                  = "Consultez le guide de l'interface utilisateur (HIG) et apprenez comment faire de votre application une expérience inédite.";
 $lang['develop_link2']                  = "Découvrez-en plus sur nos bibliothèques de programmation (API), ainsi que toutes les technologies disponibles.";
 $lang['develop_link3']                  = "Utilisez Launchpad pour gérer les rapports de bug et collaborer avec d'autres développeurs.";
 $lang['develop_link4']                  = "Configurez un compte Ubuntu My Apps et publiez vos application dans la Logithèque.";
--- francais/getinvolved_lang.php
+++ francais/getinvolved_lang.php
@@ -8,8 +8,8 @@ $lang['gi_heading2']                    = "Design";
 $lang['gi_heading3']                    = "Traductions";
 $lang['gi_heading4']                    = "Web";
 
-$lang['gi_sec1para1']                   = "Notre environnement de bureau et toutes ses applications sont dévelopés avec Vala, GTK+, Clutter, Cairo, Granite, et d'autres bibliothèques libres. Tout notre code est hébergé sur Launchpad.net, un service gratuit pour les projets libres. Nous recherchons constamment des contributeurs de tous niveaux pour nous aider à éradiquer les bugs et introduire de nouvelles fonctionnalités.";
-$lang['gi_sec1link1']                   = "Parcourir le code du Bureau";
+$lang['gi_sec1para1']                   = "Notre environnement de bureau et toutes ses applications sont dévelopés en Vala, GTK+, Clutter, Cairo, Granite, ainsi que d'autres bibliothèques libres. Tout notre code est hébergé sur Launchpad.net, un service gratuit pour les projets libres. Nous recherchons constamment des contributeurs de tous niveaux pour nous aider à éradiquer les bugs et introduire de nouvelles fonctionnalités.";
+$lang['gi_sec1link1']                   = "Parcourir le code du bureau";
 $lang['gi_sec1link2']                   = "Parcourir le code des applications";
 $lang['gi_sec1link3']                   = "Voir les rapports de bug ouverts";
 $lang['gi_sec1link4']                   = "Lire notre liste de diffusion";
@@ -23,11 +23,11 @@ $lang['gi_sec2link1']                   = "Voir notre groupe deviantART";
 $lang['gi_sec2link2']                   = "Parcourir nos plans";
 
 $lang['gi_sec3para1']                   = "elementary est créé et utilisé par des personnes du monde entier; aidez-nous à rendre meilleure l'expérience de chacun en le traduisant en plus de langues. Launchpad possède un outil intégré nommé Rosetta qui permet la traduction collaborative en ligne.";
-$lang['gi_sec3para2']                   = "Nous travaillons également à rendre le contenu de notre site web disponible en plusieur langues. Ce n'est pas disponible pour le moment mais restez connecté pour plus d'informations.";
+$lang['gi_sec3para2']                   = "Nous travaillons également à rendre le contenu de notre site web disponible en plusieurs langues. Ce n'est pas disponible pour le moment mais restez connecté pour plus d'informations.";
 $lang['gi_sec3link1']                   = "Obtenir plus d'informations à propos de Rosetta";
 $lang['gi_sec3link2']                   = "Voir notre page de traduction";
 
 $lang['gi_sec4para1']                   = "Notre site est développé en HTML, CSS, PHP, MySQL, et JavaScript. Nous recherchons constament des personnes expérimentées dans ces domaines qui souhaiteraient contribuer à l'améliorer. La plupart du travail de design est fait par notre équipe de design, mais nous apprécions toujours les retours et idées de design des développeurs web.";
 
-$lang['gi_irclink']                     = "Rejoignez nous sur IRC";
+$lang['gi_irclink']                     = "Rejoignez nous sur notre canal IRC";
 
