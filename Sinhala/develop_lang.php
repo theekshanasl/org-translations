af3cdcead78a9ad7b5d5146a4dc5d5b86d79e667
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2015-01-15 22:29:55 +0530

    Update develop_lang.php

--- Sinhala/develop_lang.php
+++ Sinhala/develop_lang.php
@@ -1,7 +1,7 @@
 <?php
-$lang['develop_title']                  = "Develop";
-$lang['develop_subtitle']               = "Your ideas into code";
-$lang['develop_parahead']               = "Resources, Info & More";
+$lang['develop_title']                  = "වැඩිදියුණු කරන්න ";
+$lang['develop_subtitle']               = "ඔබගේ අදහස් කේත මගින්";
+$lang['develop_parahead']               = "සම්පත්, තොරතුරු සහ තවත් දෑ";
 $lang['develop_paragraph']              = "Built on the best open source technology, elementary is an extremely developer-friendly platform. We provide existing and prospective developers the best available resources and documentation to ensure that your apps really shine.";
 $lang['develop_link1']                  = "Read the Human Interface Guidelines (HIG) and learn how to make your app a first-class experience.";
 $lang['develop_link2']                  = "Learn about our Application Programming Interface (APIs), and discover all the built-in technologies available to you.";
