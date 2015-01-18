ce064044d946615f817c6e15c8f2661471527596
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2015-01-18 18:35:19 +0530

    Update develop_lang.php

--- Sinhala/develop_lang.php
+++ Sinhala/develop_lang.php
@@ -6,4 +6,4 @@ $lang['develop_paragraph']              = "Built on the best open source technol
 $lang['develop_link1']                  = "Read the Human Interface Guidelines (HIG) and learn how to make your app a first-class experience.";
 $lang['develop_link2']                  = "Learn about our Application Programming Interface (APIs), and discover all the built-in technologies available to you.";
 $lang['develop_link3']                  = "Use Launchpad to manage bug reports and collaborate with other developers.";
-$lang['develop_link4']                  = "Set up an Ubuntu My Apps account and publish your apps into the Software Center.";
+$lang['develop_link4']                  = "උබුන්ටු My Apps ගිණුමක්  ආරම්භ කර ඔබගේ මෘදුකාංග Software Center හි පල කරන්න.";
